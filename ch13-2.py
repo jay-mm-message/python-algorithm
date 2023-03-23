8b135cd0e572a70442b5977709c8595cbafc2d29
jay <jay.mm.message@gmail.com>
2023-03-23 23:46:41 +0800

    Used to remove and return the last popped element from a deque (double-ended queue)

---
+++ ch13-2.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+graph = {}
+graph['Tom'] = ['Ivan', 'Ira', 'Kevin']
+people = deque()
+people += graph['Tom']
+
+print("List the data type for people: ", type(people))
+print("List search results: ", people)
+
+while len(people) != 0:
+  # Used to remove and return the last popped element from a deque (double-ended queue)
+  print(people.pop())
