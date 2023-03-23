264c6cdf4d6068e78e2cd205a4eec1f33ae26cec
jay <jay.mm.message@gmail.com>
2023-03-23 15:09:39 +0800

    Creating a hash using hashlib

---
+++ ch8-4.py
@@ -0,0 +1,12 @@
+
+import hashlib
+
+
+data = hashlib.md5()
+data.update(b"How about you today?")
+
+print('Hash Value: ', data.digest())
+print('Hash Value(Hex): ', data.hexdigest())
+
+print(type(data))
+print(type(data.hexdigest()))
