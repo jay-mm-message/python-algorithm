46932b2f8140825e0beef9c1923340ab86fbf65c
jay <jay.mm.message@gmail.com>
2023-03-23 18:08:55 +0800

    The result after using build-in sort algorithm for python

---
+++ ch9-2.py
@@ -0,0 +1,11 @@
+# Using the build-in sort algorithm for python
+
+cars = ['honda', 'bmw', 'toyota', 'ford']
+print("The result is current: {}".format(cars))
+cars.sort()
+print("The result after using build-in sort algorithm for python: {}"
+      .format(cars))
+nums = [5, 3, 9, 2]
+print("The result is current: {}".format(nums))
+nums.sort()
+print("The result after using build-in sort for python: {}".format(nums))
